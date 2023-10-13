bcf630a9644a2dff3bda7a54f5fdd9302fc29ea6
slava-vsk <146423148+slava-vsk@users.noreply.github.com>
2023-10-13 14:33:34 +0300

    Add files via upload

---
+++ cc_calc_lab3.py
@@ -0,0 +1,35 @@
+print(" !!! ВІТАЮ !!! Це примітивний калькулятор! "
+   "\n...вмію додавати, віднімати, множити, ділити...\n"
+   "\n_Для виходу з програми напишить 0_\n"
+      "\n_коли запитаю ввести знак оперцїї_\n")
+dod_arg = lambda a, b: a + b
+vid_arg = lambda a, b: a - b
+mnz_arg = lambda a, b: a * b
+dil_arg = lambda a, b: a / b
+
+ 
+
+Fl_exit = 0
+
+while Fl_exit != 1:
+    if Fl_exit == 0:
+        a = float(input("a = "))
+        b = float(input("b = "))
+        s = input("Дія (+, -, *, /): ")
+        if s == '0':
+            print(" !!! На все добре !!! ")
+            break
+        if s in ('+', '-', '*', '/'):
+            if s == '+':
+                print("%.2f" % dod_arg(a,b))
+            elif s == '-':
+                print("%.2f" % vid_arg(a,b))
+            elif s == '*':
+                print("%.2f" % mnz_arg(a,b))
+            elif s == '/':
+                if b != 0:
+                    print("%.2f" % dil_arg(a,b))
+                else:
+                    print("Ділення на нуль !!!")
+    else:
+        print("Невірна дія! Введіть + або - або * або /")
